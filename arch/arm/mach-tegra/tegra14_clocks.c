e37503179c9738f3da6378f12faf9cc2770f1983
Ivan Wong <ivan.wong@tinnomobile.com>
2014-06-20 09:41:54 +0800

    NV Patch: SL460 memory patch

--- arch/arm/mach-tegra/tegra14_clocks.c
+++ arch/arm/mach-tegra/tegra14_clocks.c
@@ -5812,7 +5812,7 @@ static struct clk tegra_dfll_cpu = {
 	.flags     = DFLL,
 	.ops       = &tegra_dfll_ops,
 	.reg	   = 0x2f4,
-	.max_rate  = 2116500000UL,
+	.max_rate  = 2014500000UL,
 };
 
 /* Audio sync clocks */
@@ -6060,7 +6060,7 @@ static struct clk tegra_clk_cclk_g = {
 	.inputs	= mux_cclk_g,
 	.reg	= 0x368,
 	.ops	= &tegra_super_ops,
-	.max_rate = 2116500000UL,
+	.max_rate = 2014500000UL,
 };
 
 static struct clk tegra_clk_cclk_lp = {
@@ -6085,7 +6085,7 @@ static struct clk tegra_clk_virtual_cpu_g = {
 	.name      = "cpu_g",
 	.parent    = &tegra_clk_cclk_g,
 	.ops       = &tegra_cpu_ops,
-	.max_rate  = 2116500000UL,
+	.max_rate  = 2014500000UL,
 	.u.cpu = {
 		.main      = &tegra_pll_x,
 		.backup    = &tegra_pll_p_out4,
@@ -6119,7 +6119,7 @@ static struct clk tegra_clk_cpu_cmplx = {
 	.name      = "cpu",
 	.inputs    = mux_cpu_cmplx,
 	.ops       = &tegra_cpu_cmplx_ops,
-	.max_rate  = 2116500000UL,
+	.max_rate  = 2014500000UL,
 };
 
 static struct clk tegra_clk_cop = {
